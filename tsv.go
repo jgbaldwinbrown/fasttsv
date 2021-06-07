7b39b9ae9311d2ef88637762ed94ae177c5b9021
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-06-07 11:12:50 -0600

    manual split working, as fast as mawk

--- tsv.go
+++ tsv.go
@@ -17,6 +17,22 @@ func rearrange_col(split_line []string, cols []int, old_rcols []string) []string
     return out
 }
 
+func manual_split(instring string, sep byte, outslice []string) []string {
+    outslice = outslice[:0]
+    start := 0
+    end := 0
+    for end = 0; end < len(instring); end++ {
+        if instring[end] == sep {
+            outslice = append(outslice, instring[start:end])
+            start = end+1
+        }
+    }
+    if start < len(instring) {
+        outslice = append(outslice, instring[start:end])
+    }
+    return outslice
+}
+
 func rearrange_cols(inconn io.Reader, outconn io.Writer, cols []int) {
     scanner := bufio.NewScanner(inconn)
     scanner.Buffer(make([]byte, 0), 10e9)
@@ -24,8 +40,9 @@ func rearrange_cols(inconn io.Reader, outconn io.Writer, cols []int) {
     b.Grow(1e6)
     rcols := make([]string, 0, 1000)
     out_lines := make([]string, 0, 20000)
+    split_line := make([]string, 0, 1000)
     for scanner.Scan() {
-        split_line := strings.Split(scanner.Text(), "\t")
+        split_line := manual_split(scanner.Text(), '\t', split_line)
         rcols = rearrange_col(split_line, cols, rcols)
         // fmt.Fprintln(&b, strings.Join(rcols, "\t"))
         // b.WriteString(strings.Join(rcols, "\t"))
